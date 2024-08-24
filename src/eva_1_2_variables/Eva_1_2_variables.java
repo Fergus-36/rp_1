20ba897a4c6014bcc603f931712398cbc3608461
Fergus-36 <L24550380@chihuahua2.tecnm.mx>
2024-08-24 00:15:08 -0600

    Add files via upload

---
+++ src/eva_1_2_variables/Eva_1_2_variables.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_2_variables;
+
+/**
+ *
+ * @author Alondra
+ */
+public class Eva_1_2_variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ System.out.println("Hello World!");//candenas de texto
+        System.out.println(100);            //entero
+        System.out.println(3.1416);         //numeros de puntos flotantes
+        System.out.println("c");            //caracter
+        System.out.println(true);           //valores logicos
+        System.out.println(false);          
+          
+           //;fin de las instrucciones
+    //enteros;
+    //int
+    //long
+    //short
+    //numeros con partes decimales(flotantes):
+    //float
+    //double
+    //cadenas de texto:
+    //string
+    //caracteres:
+    //char
+    //valores logicos:
+    //boolean
+    
+    //DELCARACION DE VARIABLE:
+    //sintaxis para declarar variable(reglas)
+    //tipo_de_dato nombre_de_la_variable;
+    String nombre;
+    String apelldio;
+    String curp;
+    float promedio;
+    }
+    
+}
