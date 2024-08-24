b01c708d90bb621b9b4d992a45fa0da1b094ab13
Fergus-36 <L24550380@chihuahua2.tecnm.mx>
2024-08-24 00:21:30 -0600

    Add files via upload

---
+++ src/eva_1_1_hola/mundo/Eva_1_1_holaMundo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_1_hola.mundo;
+
+/**
+ *
+ * @author Alondra
+ */
+public class Eva_1_1_holaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    System.out.println("Hola Mundo");
+    }
+    
+}
