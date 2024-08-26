664664d3fa08ccba2a662d05c5fc43452f2d4e52
Fergus-36 <L24550380@chihuahua2.tecnm.mx>
2024-08-26 17:53:53 -0600

    Add files via upload

---
+++ eva_1_2_variables/src/eva_1_2_variables/Eva_1_2_variables.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_2_variables;
+
+/**
+ *
+ * @author Alondra
+ */
+public class Eva_1_2_variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ System.out.println("Hello World!");//candenas de texto
+        System.out.println(100);            //entero
+        System.out.println(3.1416);         //numeros de puntos flotantes
+        System.out.println("c");            //caracter
+        System.out.println(true);           //valores logicos
+        System.out.println(false);          
+          
+           //;fin de las instrucciones
+    //enteros;
+    //int
+    //long
+    //short
+    //numeros con partes decimales(flotantes):
+    //float
+    //double
+    //cadenas de texto:
+    //string
+    //caracteres:
+    //char
+    //valores logicos:
+    //boolean
+    
+    //DELCARACION DE VARIABLE:
+    //sintaxis para declarar variable(reglas)
+    //tipo_de_dato nombre_de_la_variable;
+    String nombre;
+    String apellido;
+    String curp;
+    double promedio;
+    int edad;
+   edad=18; //ASINGACION: estoy colocando el valor 18 dentro de la variable 
+   nombre= "Maria Fernanda";
+   apellido="Hernadez vazquez";
+   promedio = 8.4; 
+   System.out.println(edad);
+   System.out.println(nombre);
+   System.out.println(apellido);
+
+
+    }
+    
+}
---
+++ eva_1_3_varoanle2/src/eva_1_3_varoanle2/Eva_1_3_varoanle2.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_3_varoanle2;
+
+/**
+ *
+ * @author Alondra
+ */
+public class Eva_1_3_varoanle2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String modelo;
+        String marca;
+        int year; //la Ñ en otros lenguajes no es apseptable
+        double precio;
+        //Asingacion 
+        marca="Nissan";
+        modelo="GTR";
+        year =2008;
+        precio= 100000.1;
+        System.out.println(marca);
+        System.out.println(modelo);
+        System.out.println(year);
+        System.out.println(precio);
+                 
+                
+    }
+    
+}
