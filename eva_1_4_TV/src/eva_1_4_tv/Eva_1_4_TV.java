ac24607153f9d8f1b9bb17d907118ae8a5d0d713
Fergus-36 <L24550380@chihuahua2.tecnm.mx>
2024-08-28 17:48:22 -0600

    Add files via upload

---
+++ eva_1_4_TV/src/eva_1_4_tv/Eva_1_4_TV.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_4_tv;
+
+/**
+ *
+ * @author Alondra
+ */
+public class Eva_1_4_TV {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    String marca;
+    String modelo;         
+    int longitud;
+    double precio;        
+    marca="LG";
+    modelo="samrt tv";
+    longitud=100;
+    precio=10000.9;
+    
+   System.out.println(marca);
+   System.out.println(modelo);         
+   System.out.println(longitud);
+   System.out.println(precio);
+
+    }
+    
+}
